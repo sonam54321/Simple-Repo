fd19248025c72da8290b0c63a05fe98e23c9d0a0
sonam <sonamramteke34@gmail.com>
2022-11-03 13:03:48 +0530

    All code added

--- Test.java
+++ Test.java
@@ -1,8 +1,19 @@
 public class Test{
 
+<<<<<<< Updated upstream
   public void add() {
    System.out.println("Add Method");
 
 }  
 
+=======
+   public static void main(String[] args) {
+
+   int a=10;
+   int b=20;
+   int c=a+b;
+
+    System.out.println(c);
+}
+>>>>>>> Stashed changes
 }
