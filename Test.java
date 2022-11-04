a4349842bad4668bbac58943ee56f7fbddd778c8
sonam54321 <116933622+sonam54321@users.noreply.github.com>
2022-11-04 16:28:07 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,19 +1,3 @@
-public class Test{
+public class Simple{
 
-<<<<<<< Updated upstream
-  public void add() {
-   System.out.println("Add Method");
-
-}  
-
-=======
-   public static void main(String[] args) {
-
-   int a=10;
-   int b=20;
-   int c=a+b;
-
-    System.out.println(c);
-}
->>>>>>> Stashed changes
 }
